b491e8d90c480a2abb58c5df5652a7e15a2d47d3
Ravindranath Edlapati <edravi@gmail.com>
2019-09-09 06:13:11 -0400

    my first program

---
+++ 01_hello.py
@@ -0,0 +1,12 @@
+print("hello World")
+
+a,b,c,d,e,f,g,h,i,j,k,l= 'C','h','e','e','s','e','b','u','r','g','e','r'
+
+print a+b+c+d+e+f,
+print g+h+i+j+k+l
+
+print 'how old are you?',
+age = raw_input()
+
+print 'so, you are %s old' %age
+
